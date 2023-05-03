edab463be305eda3078bec0647ccce178442cef9
Lucas Tymen <lucas.tymen@gmail.com>
2023-05-03 15:56:08 +0200

    04_Modules_Python_Decimals

---
+++ 04_Modules_Python_Decimals/script.py
@@ -0,0 +1,9 @@
+# Import Decimal below:
+
+
+# Fix the floating point math below:
+two_decimal_points = 0.2 + 0.69
+print(two_decimal_points)
+
+four_decimal_points = 0.53 * 0.65
+print(four_decimal_points)
