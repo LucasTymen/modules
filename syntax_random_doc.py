5dada983d8029ced2133580938b194640bbc7579
Lucas Tymen <lucas.tymen@gmail.com>
2023-05-03 18:07:45 +0200

    plt plot

--- syntax_random_doc.py
+++ syntax_random_doc.py
@@ -287,3 +287,16 @@ plt.plot(x, y)
 # Aliasing calendar as c
 import calendar as c
 print(c.month_name[1])
+
+
+########################### random.randint() and random.choice() ###########################
+
+# Returns a random integer N in a given range, such that start <= N <= end
+# random.randint(start, end)
+r1 = random.randint(0, 10)
+print(r1) # Random integer where 0 <= r1 <= 10
+
+# Prints a random element from a sequence
+seq = ["a", "b", "c", "d", "e"]
+r2 = random.choice(seq)
+print(r2) # Random element in the sequence
